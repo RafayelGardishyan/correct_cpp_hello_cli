a6373e94e588e4e8f1d9d2bdf97c05f61909a7d6
RafayelGardishyan <rgardishyan@gmail.com>
2018-01-05 21:05:07 +0100

    Working

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-main(argc, argv) 
-{
+#include <iostream>
 
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello";
+    for(int i=0; i!=argc; i++)
+    {
+       if(std::string(argv[i]) == "world"){
+           std::cout << " world";
+       }
+    }
+    std::cout << "\n";
+    return 0;
 }
